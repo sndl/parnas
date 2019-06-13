17af01f9c489fed7c03cb86fb1e24792adedd460
Stanislav Sandalnikov <stanislav.sandalnikov@jetbrains.com>
2019-06-13 16:20:32 +0300

    Add --version to CLI

--- src/main/kotlin/sndl/parnas/Main.kt
+++ src/main/kotlin/sndl/parnas/Main.kt
@@ -1,9 +1,10 @@
 package sndl.parnas
 
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.versionOption
 import sndl.parnas.cli.*
 
-fun main(args: Array<String>) = Cli()
+fun main(args: Array<String>) = Cli().versionOption("0.1.2")
         .subcommands(
                 GetParam(), SetParam(), RmParam(),
                 ListParam(), DiffParam(), UpdateParamFrom(),
