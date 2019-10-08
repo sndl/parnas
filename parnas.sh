76544886853c5c8d8f02f3907b24bacafc1212b8
Stanislav Sandalnikov <stanislav.sandalnikov@jetbrains.com>
2019-10-08 17:14:35 +0300

    Improve parnas.sh [skip ci]

--- parnas.sh
+++ parnas.sh
@@ -5,6 +5,7 @@ APP_NAME=Parnas
 LATEST_BUILD_INFO=$(curl -s https://api.github.com/repos/sndl/parnas/releases/latest)
 CHANGELOG_URL='https://github.com/sndl/parnas/blob/master/CHANGELOG.md'
 PARNAS_HOME=$HOME/.local/share/parnas
+FIRST_ARG=$1
 
 check_prerequisites () {
     local required_utils="java curl jq"
@@ -29,12 +30,18 @@ check_for_updates () {
     local latest_ver=$(echo ${LATEST_BUILD_INFO} | jq -r '.name')
     local jar_name=${PARNAS_HOME}/${APP_NAME}.jar
 
-    if [[ ${latest_ver} == "" ]]; then
+    if [[ ${latest_ver} == "" || ${latest_ver} == "null" ]]; then
         echo "WARNING: Couldn't connect to GitHub to check for updates."
         return
     fi
 
-    if [[ ${cur_ver} == "" || ${cur_ver} != ${latest_ver} || ! -f ${jar_name} ]]; then
+    if [[ ${cur_ver} == "" || ${cur_ver} != ${latest_ver} || ! -f ${jar_name} || (${FIRST_ARG} == "check-updates" && ${cur_ver} != ${latest_ver}) ]]; then
+        if [[ ${latest_ver} == $(cat ${PARNAS_HOME}/skip-update 2> /dev/null) && ${FIRST_ARG} != "check-updates" ]]
+        then
+            echo "You are using ${APP_NAME} ${cur_ver}, newever version ($(cat ${PARNAS_HOME}/skip-update)) is available but was skipped."
+            return
+        fi
+
         echo "New version of ${APP_NAME} is available - ${latest_ver}"
         echo "CHANGELOG: $CHANGELOG_URL"
         echo "Would you like to update? (yes/no):"
@@ -45,9 +52,19 @@ check_for_updates () {
             curl $(echo ${LATEST_BUILD_INFO} | jq -r '.assets[].browser_download_url') -L -s -o ${jar_name} && echo ${latest_ver} > ${PARNAS_HOME}/version
             echo "Updated!"
         else
-            echo "Update skipped."
-        fi
+            echo "${latest_ver}" > ${PARNAS_HOME}/skip-update
 
+            echo "Update to ${latest_ver} skipped."
+            echo "You can always check for updates by running \"${APP_NAME} check-updates\""
+        fi
+    else
+        echo "You are using latest version of ${APP_NAME} - ${cur_ver}"
+    fi
+
+    if [[ ${FIRST_ARG} == "check-updates" ]]
+    then
+        echo "Exiting..."
+        exit 0
     fi
 }
 
